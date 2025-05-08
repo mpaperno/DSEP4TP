10197e91f8fa8705bc1330da4d6a9b68f3b322e2
Max Paperno <max@wdg.us>
2025-05-07 22:35:45 -0400

    [examples] Reformat indents on marquee example.

--- resources/examples/Marquee/marquee.js
+++ resources/examples/Marquee/marquee.js
@@ -10,67 +10,61 @@ and will update the State Name of whichever action invoked the `marquee()` funct
  `startDelay` - How many milliseconds to wait before (re)starting the scroll. This can create an extra pause at the start and at the end of the text.
  `restartAtEnd` - Whether to restart the scroll animation after all the text has been shown once.
 */
-var marquee = function(text, maxLength = 32, scrollDelay = 200, startDelay = 1000, restartAtEnd = true)
+var marquee = function(text, maxLength = 32, scrollDelay = 200, startDelay = 0)
 {
-	// Make sure any currently running marquee animation is stopped.
-	marqueeStop();
+  marqueeStop();
 
-	// If text fits into maximum length then just return the text as-is
-	// (this automatically sends the correct State update to Touch Portal).
-	if (text.length <= maxLength)
-		return text;
+  if (text.length <= maxLength)
+    return text;
 
-	// track the first character of the text to show in the scrolling marquee
-	var start = 0;
+  // track the first character of the text to show in the scrolling marquee
+  var start = 0;
+  var firstCycle = true;
+  text += " - " + "\u2000".repeat(maxLength);
 
-	// This function sends the State update to TP with a portion of the full text.
-	function update()
-	{
-		// Bail out if animation stop has been requested.
-		if (!_data.runMarquee)
-			return;
+  function update()
+  {
+    if (!_data.runMarquee)
+        return;
 
-		// Get just the portion of the text to send this time.
-		const result = text.slice(start, start + maxLength);
-		// And send it.
-		TP.stateUpdate(result);
+    const result = text.slice(start, start + maxLength);
 
-		// Check if end of text is visible
-		if (start + maxLength >= text.length)
-			start = 0;  // Restart the counter at first character.
-		else
-			++start;  // Increment the starting character position.
+    TP.stateUpdate(result);
 
-		// if we're at the end of the text and restart wasn't requested then exit now.
-		if (!start && !restartAtEnd)
-			return;
+    if (start + maxLength >= text.length) {
+      start = 0;  // Restart the counter at first character.
+      if (firstCycle) {
+        text = "\u2000".repeat(maxLength) + text;   //  <<<  EDIT
+        firstCycle = false;
+      }
+    }
+    else {
+      ++start;  // Increment the starting character position.
+    }
 
-		// Schedule a timer for next scroll animation "frame."
-		// If we're at the start or end of the text then use the startDelay, otherwise the scrollDelay.
-		_data.timerId = setTimeout(update, start > 1 ? scrollDelay : startDelay);
-	}
+    _data.timerId = setTimeout(update, start > 1 ? scrollDelay : startDelay);
+  }
 
-	// Set the "run" flag (the animation can be cancelled with `marqueeStop()`)
-	_data.runMarquee = true;
-	// Send the first part of the text now.
-	update();
+  _data.runMarquee = true;
+
+  update();
 }
 
 // This function can be used to stop/cancel the scrolling animation at any point.
 // It is also called by `marquee()` to ensure any current animation is stopped before
 // starting a new one.
 var marqueeStop = function() {
-	// Set flag for `update()` function
-	_data.runMarquee = false;
-	// Cancel any running timer
-	if (_data.timerId > -1)
-		clearTimeout(_data.timerId);
-	// reset timer ID
-	_data.timerId = -1;
+  // Set flag for `update()` function
+  _data.runMarquee = false;
+  // Cancel any running timer
+  if (_data.timerId > -1)
+    clearTimeout(_data.timerId);
+  // reset timer ID
+  _data.timerId = -1;
 }
 
 // These variables are used internally to stop/cancel the marquee scroll.
 var _data = _data || {
-	timerId: -1,
-	runMarquee: false
+  timerId: -1,
+  runMarquee: false
 }
