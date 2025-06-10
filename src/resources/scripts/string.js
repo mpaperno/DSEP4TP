90dc156a542f9d53844ac999613372dc9f585734
Max Paperno <max@wdg.us>
2025-06-10 07:25:02 -0400

    [jslib] Add `String.elide*()` methods and static functions; Move docs to separate file; add string.js to qrc.

--- src/resources/scripts/string.js
+++ src/resources/scripts/string.js
@@ -18,96 +18,7 @@ of their respective licenses. The copyright notice above does not apply
 to any 3rd-party components used within.
 */
 
-/*!
-	\fn string arg(value)
-	\memberof String
-	Returns a copy of this string with the lowest numbered place marker replaced by value.
-	Place markers begin with a `%` symbol followed by a number in the 1 through 99 range, i.e. %1, %2, ..., %99.
-
-	The following example prints "There are 20 items":
-	```js
-	var message = "There are %1 items"
-	var count = 20
-	console.log(message.arg(count))
-	```
-	\sa `String.format()`, `sprintf()`
-
-
-
-	\fn string format(format, args)
-	\memberof String
-	Formats `format` string using the provided arguments in `args`, if any, using .NET-style formatting syntax.
-	\param format The formatting string used to format the additional arguments.
-	\param args Zero or more arguments for the format string.
-
-	Formatting is done using format strings almost completely compatible with the String.Format method in Microsoft .NET Framework.
-
-	For example:
-	```js
-	String.format(
-			"Welcome back, {0}! Last seen {1:M}",
-			"John Doe", new Date(1985, 3, 7, 12, 33)
-	);
-	// output: "Welcome back, John Doe! Last seen April 07"
-	```
-
-	There is also a convenience global function available as an alias, named simply `Format()`. It works identically to `String.format()`
-	but is shorter to type.
-
-	See formatting string reference at <br />
-	https://learn.microsoft.com/en-us/dotnet/standard/base-types/formatting-types
-
-	This extension is originally from the [String.format for JavaScript](https://github.com/dmester/sffjs) project.
-	There are some examples and references available at that site. The original `sffjs` object referenced in their documentation
-	is available in this script engine as `Sffjs`.
-
-	\sa `Number.format()`, `Date.format()`, and `sprintf()` for "printf-style" string formatting.
-
-*/
-
-/*!
-	\fn string simplified()
-	\memberof String
-	Returns this string with leading, trailing, and all redundant internal whitespace removed. contents of this byte array as a Base64-encoded string.
-	Example:
-	```js
-  var str = "  lots\t of\nwhitespace\r\n ";
-  str = str.simplified();
-  // str == "lots of whitespace";
-	```
-*/
-
-/*!
-	\fn string appendLine(String line, int maxLines, separator = '\n')
-	\memberof String
-	Appends string `line` to current string, and returns a new block with up to `maxLines` lines. 
-	If `maxLines` is zero or negative, returns the full resulting string.
-	The newline (`\n`) is used as separator by default, or a custom separator string can be specified in `separator` argument.
-*/
-
-/*!
-	\fn string appendLine(String text, String line, int maxLines, String separator = '\n')
-	\memberof String
-	\static
-	Appends string `line` to `text` and returns a new block with up to `maxLines` lines.
-	Static version of `String.prototype.appendLine()`, above. Used, for example, like this: `String.appendLine("Original string", "New line to append", 4)`
-*/
-
-/*!
-	\fn string getLines(int maxLines, int fromLine = 0, String separator = '\n')
-	\memberof String
-	Splits string `text` into lines based on `separator` and returns `maxLines` of text starting at `fromLine` (zero-based index).
-	Default `maxLines` is 1,  `fromLine` is zero (the start) and default `separator` is "\n". Specify a negative `fromLine` to count lines from the end
-	instead of the beginning (so `-1` returns one line from the end). The result may be the complete input if there were fewer then `maxLines` found in it.
-*/
-
-/*!
-	\fn string getLines(String text, int maxLines, int fromLine = 0, String separator = '\n')
-	\memberof String
-	\static
-	Splits string `text` into lines based on `separator` and returns `maxLines` of text starting at `fromLine` (zero-based index).
-	Static version of `String.prototype.getLines()`, above. Used, for example, like this: `String.getLines("Text\nWith some\nLines\n", 1, -1)`
-*/
+// documentation in ./string.dox
 
 String.prototype.simplified = function() { return Util.stringSimplify(this); };
 String.prototype.trimStart = function() { return Util.stringTrimLeft(this); };
@@ -115,5 +26,26 @@ String.prototype.trimEnd = function() { return Util.stringTrimRight(this); };
 String.prototype.appendLine = function(line, maxLines, separator = '\n') { return Util.appendLine(this, line, maxLines, separator); };
 String.prototype.getLines = function(maxLines, fromLine = 0, separator = '\n') { return Util.getLines(this, maxLines, fromLine, separator); };
 
+String.prototype.elideLeft = function(maxLen) {
+    if (this.length <= maxLen)
+        return this;
+    return "…" + this.slice(-maxLen);
+};
+
+String.prototype.elideRight = function(maxLen) {
+    if (this.length <= maxLen)
+        return this;
+    return this.slice(0, maxLen+1) + "…";
+};
+
+String.prototype.elideMiddle = function(maxLen) {
+    if (this.length <= maxLen)
+        return this;
+    return this.slice(0, Math.ceil(maxLen / 2) + 1) + "…" + this.slice(-Math.floor(maxLen / 2));
+};
+
 String.appendLine = function(text, line, maxLines, separator = '\n') { return Util.appendLine(text, line, maxLines, separator); };
 String.getLines = function(text, line, maxLines, separator = '\n') { return Util.getLines(text, maxLines, fromLine, separator); };
+String.elideLeft  = function(str, maxLen) { return String(str).elideLeft(maxLen); };
+String.elideRight = function(str, maxLen) { return String(str).elideRight(maxLen); };
+String.elideMiddle = function(str, maxLen) { return String(str).elideMiddle(maxLen); };
